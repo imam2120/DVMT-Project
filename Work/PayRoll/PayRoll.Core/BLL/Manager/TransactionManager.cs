6dd4524441fe1a29decccf0599afcf274b710b53
imam2120 <imamcpur@gmail.com>
2022-08-28 00:15:23 +0600

    #Tranaction entry

---
+++ Work/PayRoll/PayRoll.Core/BLL/Manager/TransactionManager.cs
@@ -0,0 +1,17 @@
+﻿using PayRoll.Core.BLL.Interface;
+using PayRoll.Core.DAL.Interface;
+using PayRoll.Core.Utility.DBManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayRoll.Core.BLL.Manager
+{
+    public class TransactionManager : ITransactionManager
+    {
+        private readonly ITransactionRepository _ITransactionRepository;
+        private readonly DBContext _dbContext;
+    }
+}
---
+++ Work/PayRoll/PayRoll/Controllers/TransactionController.cs
@@ -0,0 +1,52 @@
+﻿using PayRoll.Core.BLL.Interface;
+using PayRoll.Core.BLL.Manager;
+using PayRoll.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PayRoll.Controllers
+{
+    public class TransactionController : Controller
+    {
+        // GET: ProductInfo
+        ITransactionManager _iTransactionManager = new TransactionManager();
+        IProductInfoManager _iProductInfoManager = new ProductInfoManager();
+        ICommonManager commonManager = new CommonManager();
+        public ActionResult Index()
+        {
+            int screenCode = (int)Enum.Parse(typeof(ScreenList.Screens), Enum.GetName(typeof(ScreenList.Screens), ScreenList.Screens.ProductInfo));
+            var data = commonManager.GetScreenWisePermission(screenCode.ToString());
+            ViewData["Permission"] = data;
+            return View();
+        }
+        public ActionResult CreateOrUpdate(ProductInfo product)
+        {
+            var data = "";// ITransactionManager.CreateOrUpdate(product, 1);
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public JsonResult LoadAccountNO()
+        {
+            //CommonItem item = new CommonItem();
+            IEnumerable<DDLSourceModel> ddlDepartment = new List<DDLSourceModel>();
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("@QryOption", "1");
+            try
+            {
+                ddlDepartment = commonManager.GetLoadCombo(new DDLSourceModel
+                {
+                    SPName = @"USP_AccountNo",
+                    Params = dic,
+                });
+                return Json(ddlDepartment, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(ddlDepartment, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
